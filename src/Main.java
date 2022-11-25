d1ef96642595a2d0b10efb43fb99741642d9c8e2
K1ndbeetle <alx-s@bk.ru>
2022-11-25 14:30:24 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,28 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
 
         //task №2
         System.out.println("Задача №2");
         for (int i = 10; i >= 1; i--) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
 
         //task №3
         System.out.println("Задача №3");
         for (int i = 0; i <= 17; i +=2) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
 
         //task №4
         System.out.println("Задача №4");
         for (int i = 10; i >= -10; i--) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
 
         //homework2
         //task №1
@@ -41,13 +41,33 @@ public class Main {
         for (int i = 7; i <= 98; i += 7) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
 
         //task №3
         System.out.println("Задача №3");
         for (int i = 1; i <= 512; i *= 2) {
             System.out.print(" " + i);
         }
-        System.out.println(" ");
+        System.out.println();
+
+        //homework3
+        //task №1
+        System.out.println("Задача №1");
+        int partOfMoney = 29000;
+        int totalMoney = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalMoney += partOfMoney;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalMoney + " рублей");
+        }
+
+        //homework3
+        //task №2
+        System.out.println("Задача №2");
+        double totalSavingsAmount = 0;
+        for (int i = 1; i <= 12; i++) {
+            totalSavingsAmount += totalSavingsAmount /100;
+            totalSavingsAmount += partOfMoney;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSavingsAmount + " рублей");
+        }
     }
 }
