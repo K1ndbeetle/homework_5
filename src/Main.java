ad1aad9cced7a66b7bfdd7204cea3beae00d28eb
K1ndbeetle <alx-s@bk.ru>
2022-11-25 13:24:46 +0300

    Homework1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //homework1
+        //task №1
+        System.out.println("Задача №1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
+
+        //task №2
+        System.out.println("Задача №2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
+
+        //task №3
+        System.out.println("Задача №3");
+        for (int i = 0; i <= 17; i +=2) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
+
+        //task №4
+        System.out.println("Задача №4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
     }
 }
