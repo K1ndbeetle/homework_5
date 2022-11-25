c19877e6105b37690ea6ecc073e3398bfe8f1bd6
K1ndbeetle <alx-s@bk.ru>
2022-11-25 13:35:08 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,26 @@ public class Main {
             System.out.print(" " + i);
         }
         System.out.println(" ");
+
+        //homework2
+        //task №1
+        System.out.println("Задача №1");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        //task №2
+        System.out.println("Задача №2");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
+
+        //task №3
+        System.out.println("Задача №3");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.print(" " + i);
+        }
+        System.out.println(" ");
     }
 }
